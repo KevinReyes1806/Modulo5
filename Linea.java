7a05e27b377f15b8dca53121d2d38d7f914de03a
KevinReyes1806 <kvallecillo06@gmail.com>
2024-11-07 19:59:46 -0600

    Linea

---
+++ Linea.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author kvall
+ */
+public class Linea extends Formas {
+    private double largo;
+
+    public Linea(String color, double largo) {
+        super(color);
+        this.largo = largo;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    @Override
+    public void dibujar() {
+        System.out.println("Dibujando una linea de color " + getColor() + " con longitud de" + largo + "cm" );
+    }
+}
